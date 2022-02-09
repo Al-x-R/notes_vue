24838a63803034545b02c7e49a85a1c79d7d9f20
Alexander Roshchin <alexander.roshchin85@gmail.com>
2022-02-09 10:37:28 +0200

    Added notes form component

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,15 +2,18 @@
 <div class="wrapper">
   <div class="container">
     <HelloWorld msg="Welcome" />
+    <Form />
   </div>
 </div>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld';
+import Form from '../components/Notes/Form';
 export default {
   name: 'home',
   components: {
+    Form,
     HelloWorld
   }
 };
